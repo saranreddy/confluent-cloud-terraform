f9feb60fca04c72f65fe26863b206f38cca4cf27
saran reddysaran.reddy2002@gmail.com <you@example.com>
2025-04-07 11:21:43 -0500

    Update PostgreSQL sink connector configuration

--- cck/confluent/connectors.tf
+++ cck/confluent/connectors.tf
@@ -7,49 +7,35 @@ locals {
     }
     
     nonsensitive = {
-      "connector.class"          = "io.confluent.connect.jdbc.JdbcSinkConnector"
+      "connector.class"          = "PostgresSink"
       "name"                     = "${local.ck_env_name}-aurora-postgres-sink"
+      "input.data.format"        = "AVRO"
+      "kafka.api.key"            = confluent_api_key.postgres-api-key.id
+      "kafka.api.secret"         = confluent_api_key.postgres-api-key.secret
       "topics"                   = "unified_records_test"
       "connection.host"          = local.env_config.postgres_host
       "connection.port"          = local.env_config.postgres_port
       "connection.user"          = local.env_config.postgres_user
-      "database.name"            = local.env_config.postgres_database
-      
-      # Connection settings
-      "ssl.mode"                 = "prefer"
-      "input.data.format"        = "AVRO"
+      "db.name"                 = local.env_config.postgres_database
       
       # Table settings
       "insert.mode"              = "INSERT"
-      "table.name.format"        = "\"stg\".sor_data"
-      "table.types"              = "TABLE"
-      "pk.mode"                  = "none"
+      "table.name.format"        = "stg.sor_data"
       "auto.create"              = "false"
-      "auto.add.columns"         = "false"
-      "delete.on.null"           = "false"
+      "auto.evolve"             = "false"
       
       # Performance tuning
       "tasks.max"                = "1"
       "batch.size"               = "3000"
-      "quote.sql.identifiers"    = "ALWAYS"
-      "schema.context"           = "default"
-      "max.poll.interval.ms"     = "300000"
-      "max.poll.records"         = "500"
-      
-      # Timezone settings
-      "database.timezone"        = "UTC"
-      "timezone.used.for.date"   = "DB_TIMEZONE"
       
       # Error handling
       "errors.tolerance"         = "all"
       "errors.deadletterqueue.topic.name" = "${local.ck_env_name}-postgres-sink-dlq"
-      "errors.deadletterqueue.topic.replication.factor" = "3"
       
       # Transformations
-      "transforms"                               = "transform_0"
-      "transforms.transform_0.type"              = "org.apache.kafka.connect.transforms.InsertField$Value"
-      "transforms.transform_0.timestamp.field"   = "kafka_outbound_timestamp"
-      "transforms.transform_0.replace.null.with.default" = "true"
+      "transforms"                               = "InsertField"
+      "transforms.InsertField.type"             = "org.apache.kafka.connect.transforms.InsertField$Value"
+      "transforms.InsertField.timestamp.field"  = "kafka_outbound_timestamp"
     }
   }
 }
@@ -72,6 +58,7 @@ resource "confluent_connector" "postgres_sink" {
 
   depends_on = [
     confluent_kafka_topic.topics,
-    confluent_role_binding.postgres_admin
+    confluent_role_binding.postgres_admin,
+    confluent_api_key.postgres-api-key
   ]
 } 
